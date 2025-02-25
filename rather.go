851afce382bc3be8301ef36e9b0aa4460b09e994
baosang156 <leebaarloow22g@hotmail.com>
2025-02-25 16:09:15 +0800

    Modify rather.go

--- rather.go
+++ rather.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Want listen for expect thank consumer step from.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
